e764edbd79a0af2a6ee7247a12e7b7599e3e6810
data-nihilist <mattmccredy@gmail.com>
2023-09-21 17:59:45 -0400

    first commit

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+        Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+        Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+        Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+
+        // Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        // Task<ServiceResponse<Character>> GetCharacterById(int id);
+        // Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+    }
+}
